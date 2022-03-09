e23ccbab49de54fca2bc578f5a3c2063accb745c
enkhriiileeenn <99162318+enkhriiileeenn@users.noreply.github.com>
2022-03-09 13:43:55 +0800

    vars

--- room_service.py
+++ room_service.py
@@ -1,9 +1,11 @@
 """
 enkhriilen and dari 
 """
-guests = {'201':'Jay', '202':'Yeonjun','203':"Jimin"}
+Room service: 
+Console chatbot to order room service food from a menu.
+guests = {'201':'Jay', '202':'Yeonjun','203':'Jimin'}
 
 #Dictionaries with the menu
-starters = {'301':"tteokbokki", '302':'Gimbap'}
-
-#Menu
+starters = {'301':"tteokbokki", '302':'Gimbap', '303':'Kimchi'}
+maincourses = {'401':'Bibimbap','402':"Kimchi Stew", '403':'Army Stew'}
+desserts = {'501':'Songpyeon', '502':'Dasik', '503': 'Sikhye'} 
